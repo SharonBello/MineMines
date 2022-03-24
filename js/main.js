280baed7c2fd0eb4f1b17d5f26a142cb605c7125
sharonbello <sharonbellotech@gmail.com>
2022-03-24 21:03:43 +0200

    second day

--- js/main.js
+++ js/main.js
@@ -2,25 +2,27 @@
 
 const N = 10;
 
-const RESTART = '&#128515';
-const WINNER = '&#127942';
-const LOSER = '&#9760';
+const WINNER_IMG = '&#127942';
 
-const LIFE = '&#10084';
-const FLOOR = '';
+const LIFE_IMG = '&#10084';
+const MINE_IMG = '&#128163';
+const HIDDEN_IMG = '🟦';
+const FLAG_IMG = '&#9873';
 
-const MINE = '&#128163';
-// const MINE = '&#128165';
-const EMPTY = '';
-const FLAG = '&#9873';
-const NUMBER = 1;
-// const HINT = '&#128161';
-// const SAFE = '&#129686';
-// const MANUAL = '&#128075';
-// const SEVEN_BOOM = '&#10128;';
-// const UNDO = '&#129318';
-// const BEST_SCORE = '&#128285';
+const HIDDEN = 'hidden';
+const MINE = 'mine';
+const MARKED = 'marked';
+const NUMBER = 'number';
 
+var gBoard;
+var gScore = 0;
+var gWinScore = 0;
+var gLives = 3;
+var gLivesLost = 0;
+var gClickedTile;
+var gOpenTilesCount = 0;
+var gMines = [];
+var gFirstClick = 0;
 
 var gGame = {
     score: 0,
@@ -29,314 +31,158 @@ var gGame = {
     secsPassed: 0
 }
 
-var gDiffLevel = {
-    size: N,
-    mines: getRandomIntInclusive(2, N / 2),
-    lives: 3
-}
+var gDiffLevel;
 
-var gBoard = [];
-var gTimeCount = 0;
-var gIntervalTime = 0;
-var gScore = 0;
-var cellCount = 0;
-var gLives = 3;
-
-// This is called when page loads
 function init() {
-    // TODO: Check Level size - if won before
-    gBoard = buildBoard()
-    printMat(gBoard, '.board-container')
-    setMinesAtRandom(gBoard, gDiffLevel.mines);
-    // renderTemp
+    gDiffLevel = {
+        size: N,
+        mines: Math.floor(getRandomIntInclusive(2, N / 2)),
+        lives: 3
+    }
+    gBoard = buildBoard();
+    printMat(gBoard, '.board');
+    getMinesRandPos(gBoard, gDiffLevel.mines);
+    addMines(gBoard);
+    addNeighborsCount(gBoard);
+    renderBoard(gBoard);
     gGame.isOn = true;
-    gLives = 3;
-    // console.log('gLives', gLives);
-    console.table('gBoard', gBoard);
-    // TODO: Do I need to reset the restart emoji?
 }
 
-// Builds the board 
-// Return the created board
+function addNeighborsCount(gBoard) {
+    for (var i = 0; i < gBoard.length; i++) {
+        for (var j = 0; j < gBoard[0].length; j++) {
+            gBoard[i][j].minesAroundCount = findNeighborsCount(gBoard, i, j);
+        }
+    }
+}
 
 function buildBoard() {
     var SIZE = N;
     var board = [];
     for (var i = 0; i < SIZE; i++) {
-        var row = [];
+        board[i] = [];
         for (var j = 0; j < SIZE; j++) {
-            // Start with cell so we can use it to change status afterwards
-            var cell = {
-                minesAroundCount: 4,
-                isShown: true,
+            var tile = {
+                minesAroundCount: 0,
+                isShown: false,
                 isMine: false,
-                isMarked: true,
-                type: FLOOR,
-                gameElement: NUMBER
+                isMarked: false,
             }
-            row.push(cell);
+            board[i].push(tile);
         }
-        board.push(row);
+        board.push(board[i]);
     }
-    console.table('board', board);
     return board;
 }
 
-
-// render the board in table
-
-function renderBoard(board) {
-    var strHTML = ''
-    for (var i = 0; i < board.length; i++) {
-        strHTML += '<tr>'
-        for (var j = 0; j < board[0].length; j++) {
-            var cell = board[i][j]
-            strHTML += '<tr>\n';
-            var currCell = board[i][j];
-            var cellClass = getClassName({
+function renderBoard(gBoard) {
+    var strHTML = '';
+    for (var i = 0; i < gBoard.length; i++) {
+        strHTML += `<tr>\n`
+        for (var j = 0; j < gBoard[0].length; j++) {
+            var tileClass = getClassName({
                 i: i,
                 j: j
             })
-            cellClass += (currCell.type === FLOOR) ? ' floor' : 'wall'
-            strHTML += '<td class="cell ' + cellClass + '"  onclick="openCell(' + i + ',' + j + ')">'
-            switch (currCell.gameElement) {
-                case EMPTY:
-                    strHTML += EMPTY;
-                    break;
-                case MINE:
-                    strHTML += MINE;
-                    break;
-                case NUMBER:
-                    strHTML += NUMBER;
-                    break;
-                case FLAG:
-                    strHTML += FLAG;
-                    break;
-            }
-            strHTML += '</tr>';
+
+            strHTML += `\t<td class="tile ${tileClass}" 
+            onclick="tileClicked(this, ${i}, ${j})">
+            </td>\n`
         }
-        var elBoard = document.querySelector('.board');
-        elBoard.innerHTML = strHTML;
+        strHTML += `</tr>\n`
     }
+    var elBoard = document.querySelector('.board');
+    elBoard.innerHTML = strHTML;
 }
 
 
+function tileClicked(elTile, i, j) {
+    var tile = gBoard[i][j]
+    gBoard[i][j].isShown = true;
+    gBoard[i][j].minesAroundCount = findNeighborsCount(gBoard[i][j]);
 
-
-// Set mines at random locations 
-function setMinesAtRandom(board, minesCount) {
-    var minesPositions = getMinesAtRandom(board, minesCount);
-    for (var i = 0; i < minesPositions.length; i++) {
-        board[minesPositions[i].i][minesPositions[i].j].isMine = true;
+    if (gBoard[i][j].isMine) {
+        elTile.classList.add('mine')
+        appendElementImg(elTile, MINE_IMG);
+        gDiffLevel.lives--;
+        gameOver()
     }
+
+    if (!gBoard[i][j].isMarked) elTile.classList.add('marked');
 }
 
-// Return array of random board positions each an object of i, j,
-// Receives the number of  X amount of mines that is calculated randomly min 2, max N/5
-function getMinesAtRandom(board, minesCount) {
-    var randPositions = [];
+function getMinesRandPos(board, minesCount) {
+    var randPos;
     for (var i = 0; i < minesCount; i++) {
-        var randomI = getRandomInt(0, board[0].length);
-        var randomJ = getRandomInt(0, board[0].length);
-        randPositions.push({
-            i: randomI,
-            j: randomJ
-        });
+        for (var j = 0; j < minesCount; j++) {
+            var randomI = getRandomInt(0, board[0].length);
+            var randomJ = getRandomInt(0, board[0].length);
+            randPos = ({
+                i: randomI,
+                j: randomJ
+            });
+        }
+        gMines.push(randPos);
     }
-    console.log('randPos', randPositions);
-    return randPositions;
+    return gMines;
 }
 
-// Reflect change of score in DOM
-function updateScore(diff) {
-    gGame.score += diff;
-    document.querySelector('.score span').innerText = gGame.score;
+function addMines() {
+    var tiles = getMinesRandPos();
+    for (var i = 0; i < tiles.length; i++) {
+        var tile = tiles[i];
+        gBoard[tile.i][tile.j].isMine = true;
+    }
+    console.log('tiles', tiles);
 }
 
-// Count mines around each cell 
-// Call setMinesNegsCount() 
-
-// set the cell's minesAroundCount.
-// function setMinesNegsCount(board) {
-
-// }
-
-// renderBoard(board)
-// Render the board as a <table> to the page
-
-// cellClicked(elCell, i, j)
-// Called when a cell (td) is clicked
-
-// cellMarked(elCell)
-// Called on right click to mark a cell (suspected to be a mine) 
-// Search the web (and implement) how to hide the context menu on right click
-
-// checkGameOver()
-// Game ends when all mines are marked, and all the other cells are shown
-
-// expandShown(board, elCell, i, j)
-// When user clicks a cell with no mines around, we need to open not only that cell, but also its neighbors. 
-// NOTE: start with a basic implementation that only opens the non-mine 1st degree neighbors 
-// BONUS: if you have the time later, try to work more like the real algorithm (see description at the Bonuses section below)
-
-// The model
-// gBoard – A Matrix containing cell objects: 
-// Each cell: { 
-//     minesAroundCount: 4, 
-//     isShown: true, 
-//     isMine: false, 
-//     isMarked: true 
-// }
-
-// This is an object by which the board size is set (in this case: 4x4 board and how many mines to put)
-// gLevel = { 
-//     SIZE: 4, 
-//     MINES: 2 
-// };
-
-
-// This is an object in which you can keep and update the current game state: isOn: Boolean, when true we let the user play shownCount: How many cells are shown markedCount: How many cells are marked (with a flag)
-// secsPassed: How many seconds passed
-// gGame = { 
-//     isOn: false, 
-//     shownCount: 0, 
-//     markedCount: 0, 
-//     secsPassed: 0 
-// }
-
-// Step1 – the seed app:
-// 1. Create a 4x4 gBoard Matrix containing Objects. Place 2 mines manually when each cell’s isShown set to true.
-// 2. Present the mines using renderBoard() function.
-// Step2 – counting neighbors:
-// 1. Create setMinesNegsCount() and store the numbers (isShown is still true)
-// 2. Present the board with the neighbor count and the mines using renderBoard() function.
-// 3. Have a console.log presenting the board content – to help you with debugging
-// Step3 – click to reveal:
-// 1. Make sure your renderBoard() function adds the cell ID to each cell and onclick on each cell calls cellClicked() function.
-// 2. Make the default “isShown” to be “false”
-// 3. Implement that clicking a cell with “number” reveals the number of this cell
-// Step4 – randomize mines' location:
-// 1. Randomly locate the 2 mines on the board
-// 2. Present the mines using renderBoard() function.
-// Next Steps: Head back to Functionality and Features and then on to Further Tasks, and if time permits check out the Bonus Tasks section.
-
-// // UI Guidelines
-// This sprint is not a UI-centered project. However, we recommend to try implementing the following UI concepts:
-// 1. Board is square and cells are squares
-// 2. Cells keep their size when hovered and when revealed
-// 3. Board keeps its position (shouldn't move) along all game phases (do not add UI elements dynamically above it)
-// 4. Mines look like mines.
-// 5. Add a footer at the bottom of the page with your full name.
-
-
-// Further Tasks
-// First click is never a Mine
-// Make sure the first clicked cell is never a mine (like in the real game)
-// HINT: place the mines and count the neighbors only on first click.
-// Lives
-// Add support for “LIVES” -
-// The user has 3 LIVES:
-
-// When a MINE is clicked, there is an indication to the user that he clicked a mine. The LIVES counter decrease. The user can continue playing.
-
-
-// Add smiley (feel free to switch icons \ images):
-// ● Normal 😃
-// ● Sad & Dead – LOSE 🤯 (stepped on a mine)
-// ● Sunglasses – WIN 😎
-// ● Clicking the smiley should reset the game
-
-// Add support for HINTS
-// The user has 3 hints
-
-// When a hint is clicked, it changes its look, example:
-// Now, when a cell (unrevealed) is clicked, the cell and its neighbors are revealed for a second, and the clicked hint disappears.
-// Best Score
-// Keep the best score in local storage (per level) and show it on the page
-// Full Expand
-// When an empty cell is clicked, open all empty cells that are connected and their numbered neighbors (as is done at the game) this feature is normally implemented using recursion.
-
-// Safe click
-// Add a Safe-Click Button:
-// The user has 3 Safe-Clicks
-// Clicking the Safe-Click button will mark a random covered cell (for a few seconds) that is safe to click (does not contain a MINE).
-// Present the remaining Safe-Clicks count
-
-
-// Manually positioned mines
-// Create a “manually create” mode in which user first positions the mines (by clicking cells) and then plays.
-// Undo
-// Add an “UNDO” button, each click on that button takes the game back by one step (can go all the way back to game start).
-
-
-// 7 BOOM!
-// Add an “7 BOOM!” button, clicking the button restarts the game and locate the MINES according to the “7 BOOM” principles (each cell-index that contains “7” or a multiplication of “7”). Note that the cell-index shall be a continuous number (i.e. in a 8*8 Matrix is shall be between 0 to 63).
-
-// function updateScore(diff) {
-//     gGame.score += diff;
-//     document.querySelector('h2 span').innerText = gGame.score;
-
-// }
-
-// function checkVictory() {
-//     if (gCountFood === gCountEat) gameOver('Victory');
-// }
-
-// function gameOver(msg) {
-//     gGame.isOn = false;
-//     clearInterval(gIntervalGhosts);
-//     clearInterval(gIntervalCherry);
-//     gBoard[gPacman.location.i][gPacman.location.j] = EMPTY;
-//     renderCell(gPacman.location, EMPTY);
-//     openModal(msg)
-// }
-
-// function openModal(msg) {
-//     var elModal = document.querySelector('.modal')
-//     elModal.style.display = 'block'
-//     var elModalH2 = elModal.querySelector('h2')
-//     var modalText = `${msg} \n Youre score is: ${gGame.score}`
-//     elModalH2.innerText = modalText
-// }
-
-// function closeModal() {
-//     var elModal = document.querySelector('.modal')
-//     elModal.style.display = 'none'
-// }
-
-// function addCherry() {
-//     var emptyLocations = getEmptyLocations();
-//     if (!emptyLocations.length) return
-//     var emptyLocation = getRandomEmptyLocation(emptyLocations)
-//     gBoard[emptyLocation.i][emptyLocation.j] = CHERRY;
-//     renderCell(emptyLocation, CHERRY)
-// }
-
-// function getRandomEmptyLocation(emptyLocations) {
-//     var randomIndex = getRandomInt(0, emptyLocations.length)
-//     var emptyLocation = emptyLocations[randomIndex];
-//     return emptyLocation
-// }
-
-// function getEmptyLocations() {
-//     var emptyLocations = []
-//     for (var i = 1; i < gBoard.length; i++) {
-//         for (var j = 1; j < gBoard[i].length; j++) {
-//             if (gBoard[i][j] === EMPTY) {
-//                 emptyLocations.push({ i: i, j: j });
-//             }
-//         }
-//     }
-//     return emptyLocations;
-// }
-
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min;
+function findNeighborsCount(board, rowIdx, colIdx) {
+    var count = 0;
+    var mineTiles = getMinesRandPos();
+    // console.log('mineTile', mineTiles);
+    for (var i = 0; i < mineTiles.length; i++) {
+        var rowIdx = (mineTiles[i]);
+        // console.log('rowIdx', rowIdx);
+        var indexes = Object.entries(rowIdx);
+        // console.log('indexes', indexes);
+        rowIdx = indexes[0][1];
+        // console.log('rowIdx', rowIdx);
+        var colIdx = indexes[1][1];
+        // console.log('colIdx', colIdx);
+    }
+    for (var i = rowIdx - 1; i <= rowIdx + 1; i++) {
+        if (i < 0 || i > board.length - 1) continue;
+        for (var j = colIdx - 1; j <= colIdx + 1; j++) {
+            if (j < 0 || j > board.length - 1) continue;
+            if (i === rowIdx && j === colIdx) continue;
+            var currTile = gBoard[rowIdx][colIdx];
+            if (currTile.isMine) count++;
+        }
+    }
+    // console.log('count', count);
+    return count;
 }
 
-function getClassName(location) {
-    var cellClass = 'cell-' + location.i + '-' + location.j;
-    return cellClass;
+function gameOver() {
+    if (gDiffLevel.lives === 0) {
+        alert('Game Over');
+        init()
+    }
+}
+
+function livesStatus(num) {
+    for (var i = 0; i < gBoard.length; i++) {
+        var currTile = gBoard[i];
+        for (var j = 0; j < currTile.length; j++) {
+            // currTile = gBoard[i][j];
+            currTile = tileClicked(gClickedTile, gBoard[i][j]);
+        }
+    }
+    if (currTile.isMine === true) {
+        var elLives = document.querySelector('.lives');
+        appendElementImg(elLives, LIFE_IMG);
+        if (num !== 0) elLives.innerHTML = LIFE_IMG * num;
+        else elLives.innerHTML = LOSE_IMG;
+        console.log('elLives', elLives);
+    }
 }
--- js/util.js
+++ js/util.js
@@ -1,6 +1,14 @@
+
+function appendElementImg(parentElem, img) {
+  var elImg = document.createElement('span')
+  elImg.innerHTML = img;
+  parentElem.appendChild(elImg);
+}
+
+
 function printMat(mat, selector) {
-    var strHTML = '<table border="0"><tbody>';
-    for (var i = 0; i < mat.length; i++) {
+  var strHTML = '<table border="0"><tbody>';
+  for (var i = 0; i < mat.length; i++) {
       strHTML += '<tr>';
       for (var j = 0; j < mat[0].length; j++) {
         var cell = mat[i][j];
@@ -14,6 +22,17 @@ function printMat(mat, selector) {
     elContainer.innerHTML = strHTML;
   }
   
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function getClassName(pos) {
+    var tileClass = 'tile-' + pos.i + '-' + pos.j;
+    return tileClass;
+}
+
   // location such as: {i: 2, j: 7}
   function renderCell(location, value) {
     // Select the elCell and set the value
@@ -28,11 +47,11 @@ function printMat(mat, selector) {
   function createMat(ROWS, COLS) {
     var mat = []
     for (var i = 0; i < ROWS; i++) {
-        var row = []
-        for (var j = 0; j < COLS; j++) {
-            row.push('')
-        }
-        mat.push(row)
+      var row = []
+      for (var j = 0; j < COLS; j++) {
+        row.push('')
+      }
+      mat.push(row)
     }
     return mat
   }
@@ -44,29 +63,20 @@ function printMat(mat, selector) {
     return Math.floor(Math.random() * (max - min)) + min;
   }
   
-  // Get arr with nums
-  function getNums(size) {
-    var nums = [];
-    for (var i = 0; i < size; i++) {
-        nums.push(i + 1);
-    }
-    return nums;
-  }
+  // const HINT = '&#128161';
+  // const SAFE = '&#129686';
+  // const MANUAL = '&#128075';
+  // const SEVEN_BOOM = '&#10128;';
+  // const UNDO = '&#129318';
+  // const BEST_SCORE = '&#128285';
+  // const MINE = '&#128165';
+  // const FLAG = '&#9873';
+  // const FLOOR = '';
+  // const EMPTY = '';
   
-  //Make Sound
-  function sound() {
-    var audio = new Audio('sound/collect.mp3')
-    audio.play()
+  function renderCell(position, value) {
+      var cellSelector = '.' + getClassName(position) // '.cell-2-5'
+      var elCell = document.querySelector(cellSelector);
+      elCell.innerHTML = value;
   }
-  
-  // get random Color
-  function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  }
-  
   
